7c43285489847fdb17144d000d7d613c5e57045e
unknown <filip.korycki2222@gmail.com>
2022-05-29 16:15:43 +0200

    Pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,35 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import * as ROUTES from './constants/routes';
+import { Home } from "./pages/Home";
+import { Signin } from "./pages/Signin";
+import { Signup } from "./pages/Signup";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route
+          path={ROUTES.HOME}
+          element={
+            <Home />
+          }
+        />
+        <Route
+          path={ROUTES.SIGNIN}
+          element={
+            <Signin />
+          }
+        />
+        <Route
+          path={ROUTES.SIGNUP}
+          element={
+            <Signup />
+          }
+        />
+
+      </Routes>
+    </Router>
   );
 }
 
